6c39e89f0f73ca5495b7eb251fa7502fb9b39e12
ko-nosuke <e155725@ie.u-ryukyu.ac.jp>
2018-11-26 15:14:37 +0900

    Repot4の１

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 public class Main {
 
-    public static void main(String[] args){
-        String str = null;
-        System.out.println(str.length());
+    public static void main(String[] args) {
+        try {
+            String str = null;
+            System.out.println(str.length());
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerExceptionを確認");
+            System.out.println(e.getMessage());
+        }
+
     }
 }
+
+
