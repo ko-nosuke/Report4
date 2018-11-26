ff98b271a62dc7baa2ba08a8cec982212ace3100
ko-nosuke <e155725@ie.u-ryukyu.ac.jp>
2018-11-26 15:38:16 +0900

    ステップ３

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
-        try {
+     /*   try {
             String str = null;
             System.out.println(str.length());
         } catch (NullPointerException e) {
             System.out.println("NullPointerExceptionを確認");
             System.out.println(e.getMessage());
-        }
+        }ステップ２までの修正はコメントアウトした。
+     */
+
+     String str = "百二十三";
+     int value = Integer.parseInt(str);
 
     }
 }
